f83f8ca21e699855d5231ecce07b854b76181baa
hectoruhalte <41273580+hectoruhalte@users.noreply.github.com>
2019-04-16 17:07:13 +0100

    Add files via upload
    Crear las principales cajas contenedoras. Incluir el logo en la caja inferior derecha

--- gtkada/teseo.adb
+++ gtkada/teseo.adb
@@ -20,6 +20,7 @@ with Gtkada.Style;
 with Gtk.Box;
 with Gtk.Widget;
 with Gtk.Frame;
+with Gtk.Image;
 
 procedure teseo is
 
@@ -28,9 +29,11 @@ procedure teseo is
 
 	V_MARCO_PRINCIPAL : Gtk.Frame.Gtk_Frame;
 
-	-- Variable para la caja principal. Hijo de la ventana principal.
+	-- Variable para la caja principal. Hijo del marco principal.
 	-- La caja contenedora sera la que contenga a las demas y tendra un marco dibujado.
-	--V_CAJA_PRINCIPAL : Gtk.Box.Gtk_Box;
+	V_CAJA_PRINCIPAL, V_CAJA_IZQUIERDA, V_CAJA_DERECHA, V_CAJA_MAPA, V_CAJA_LOGO : Gtk.Box.Gtk_Box;
+
+	V_LOGO : Gtk.Image.Gtk_Image;
 
 begin
 
@@ -65,13 +68,94 @@ begin
 			    Name => "marcoPrincipal");
 
 	Gtk.Frame.Set_Border_Width (Container => V_MARCO_PRINCIPAL,
-				    Border_Width => 10);
+				    Border_Width => 5);
 
 	--Gtk.Window.Fullscreen (V_VENTANA_PRINCIPAL);
 
-	-- Añadir la caja principal a la ventana principal
+	-- Añadir el marco principal a la ventana principal
 	Gtk.Window.Add (Container => V_VENTANA_PRINCIPAL,
 			Widget => V_MARCO_PRINCIPAL);
+
+	-- Crear la caja principal
+	Gtk.Box.Gtk_New_HBox (Box => V_CAJA_PRINCIPAL,
+			      Homogeneous => False,
+			      Spacing => 1);
+
+	-- Dar nombre al widget para ser usado en el fichero css
+	Gtk.Box.Set_Name (Widget => V_CAJA_PRINCIPAL,
+			  Name => "cajaPrincipal");
+
+	-- Añadir la caja principal al marco principal
+        Gtk.Frame.Add (Container => V_MARCO_PRINCIPAL,
+                       Widget => V_CAJA_PRINCIPAL);
+
+	-- Crear las cajas izquierda y derecha
+	Gtk.Box.Gtk_New_VBox (Box => V_CAJA_IZQUIERDA,
+			      Homogeneous => False,
+			      Spacing => 1);
+
+	Gtk.Box.Gtk_New_VBox (Box => V_CAJA_DERECHA,
+			      Homogeneous => False,
+			      Spacing => 3);
+
+	Gtk.Box.Set_Name (Widget => V_CAJA_IZQUIERDA,
+			  Name => "cajaIzquierda");
+
+	Gtk.Box.Set_Name (Widget => V_CAJA_DERECHA,
+			  Name => "cajaDerecha");
+	
+	Gtk.Box.Set_Border_Width (Container => V_CAJA_IZQUIERDA,
+                                  Border_Width => 1);
+
+	Gtk.Box.Set_Border_Width (Container => V_CAJA_DERECHA,
+                                  Border_Width => 1);
+
+	Gtk.Box.Set_Size_Request (Widget => V_CAJA_DERECHA,
+				  Width => 300);
+
+	Gtk.Box.Add (Container => V_CAJA_PRINCIPAL,
+		     Widget => V_CAJA_IZQUIERDA);
+
+	Gtk.Box.Add (Container => V_CAJA_PRINCIPAL,
+		     Widget => V_CAJA_DERECHA);
+
+	Gtk.Box.Gtk_New_HBox (Box => V_CAJA_MAPA,
+			      Homogeneous => True,
+			      Spacing => 0);
+
+	Gtk.Box.Gtk_New_HBox (Box => V_CAJA_LOGO,
+			      Homogeneous => False,
+			      Spacing => 0);
+
+	Gtk.Box.Set_Name (Widget => V_CAJA_MAPA,
+			  Name => "cajaMapa");
+
+	Gtk.Box.Set_Name (Widget => V_CAJA_LOGO,
+			  Name => "cajaLogo");
+
+	Gtk.Image.Gtk_New (Image => V_LOGO,
+			   Filename => "Dedalos_logo.png");
+
+	Gtk.Box.Pack_End (In_Box => V_CAJA_LOGO,
+			  Child => V_LOGO,
+			  Expand => False,
+			  Fill => False,
+			  Padding => 10);
+	
+	Gtk.Box.Set_Size_Request (Widget => V_CAJA_MAPA,
+				  Height => 625);
+
+	Gtk.Box.Pack_Start (In_Box => V_CAJA_DERECHA,
+			    Child => V_CAJA_MAPA,
+			    Expand => True,
+			    Fill => True,
+			    Padding => 0);
+
+	Gtk.Box.Pack_End (In_Box => V_CAJA_DERECHA,
+			  Child => V_CAJA_LOGO,
+			  Expand => True,
+			  Fill => True,
+			  Padding => 0);
 
 	--  Here we connect the "destroy" event to a signal handler.
    	--  This event occurs when we call Gtk.Widget.Destroy on the window,
