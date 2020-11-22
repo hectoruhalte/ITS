ff8ae52255b2374ae6e3963f44184169d312a950
hectoruhalte <41273580+hectoruhalte@users.noreply.github.com>
2020-11-22 10:46:42 +0000

    Add files via upload
    Actualizar definición del trayecto para tener el id del tramo actual en el tipo trayecto

--- dinamico/q_trayecto.adb
+++ dinamico/q_trayecto.adb
@@ -10,208 +10,232 @@
 
 with Q_RESTRICCION;
 
-with Ada.Text_Io;
-
 package body Q_TRAYECTO is
 
-	-------------------------------------------------------------------
-	function F_OBTENER_ID (V_TRAYECTO : in T_TRAYECTO) return String is
+   -------------------------------------------------------------------
+   function F_OBTENER_ID (V_TRAYECTO : in T_TRAYECTO) return String is
 
-	begin
+   begin
 		
-		return V_TRAYECTO.R_ID;
+      return V_TRAYECTO.R_ID;
 
-	end F_OBTENER_ID;
-	-------------------------------------------------------------------
+   end F_OBTENER_ID;
+   -------------------------------------------------------------------
 
-	--------------------------------------------------------------------------------------------------------
-	function F_OBTENER_POSICION_ORIGEN (V_TRAYECTO : in T_TRAYECTO) return Q_TIPOS_BASICOS.T_POSICION_UTM is
+   --------------------------------------------------------------------------------------------------------
+   function F_OBTENER_POSICION_ORIGEN (V_TRAYECTO : in T_TRAYECTO) return Q_TIPOS_BASICOS.T_POSICION_UTM is
 
-	begin
+   begin
 
-		return V_TRAYECTO.R_POSICION_ORIGEN;
+      return V_TRAYECTO.R_POSICION_ORIGEN;
 
-	end F_OBTENER_POSICION_ORIGEN;
-	--------------------------------------------------------------------------------------------------------
+   end F_OBTENER_POSICION_ORIGEN;
+   --------------------------------------------------------------------------------------------------------
 
-	-------------------------------------------------------------------------------------------------------
-        function F_OBTENER_POSICION_FINAL (V_TRAYECTO : in T_TRAYECTO) return Q_TIPOS_BASICOS.T_POSICION_UTM is
+   -------------------------------------------------------------------------------------------------------
+   function F_OBTENER_POSICION_FINAL (V_TRAYECTO : in T_TRAYECTO) return Q_TIPOS_BASICOS.T_POSICION_UTM is
 
-        begin
+   begin
 
-                return V_TRAYECTO.R_POSICION_FINAL;
+      return V_TRAYECTO.R_POSICION_FINAL;
 
-        end F_OBTENER_POSICION_FINAL;
-        -------------------------------------------------------------------------------------------------------
+   end F_OBTENER_POSICION_FINAL;
+   -------------------------------------------------------------------------------------------------------
 	
-	---------------------------------------------------------------------------------------
-	function F_OBTENER_HORA_SALIDA (V_TRAYECTO : in T_TRAYECTO) return Ada.Calendar.Time is
+   ---------------------------------------------------------------------------------------
+   function F_OBTENER_HORA_SALIDA (V_TRAYECTO : in T_TRAYECTO) return Ada.Calendar.Time is
 
-	begin
+   begin
 
-		return V_TRAYECTO.R_HORA_SALIDA;
+      return V_TRAYECTO.R_HORA_SALIDA;
 
-	end F_OBTENER_HORA_SALIDA;
-	---------------------------------------------------------------------------------------
+   end F_OBTENER_HORA_SALIDA;
+   ---------------------------------------------------------------------------------------
 
-	----------------------------------------------------------------------------------------------
-	function F_OBTENER_PROGRESION (V_TRAYECTO : in T_TRAYECTO) return Q_PROGRESION.T_PROGRESION is
+   ----------------------------------------------------------------------------------------------
+   function F_OBTENER_PROGRESION (V_TRAYECTO : in T_TRAYECTO) return Q_PROGRESION.T_PROGRESION is
 
-	begin
+   begin
 
-		return V_TRAYECTO.R_PROGRESION;
+      return V_TRAYECTO.R_PROGRESION;
 		
-	end F_OBTENER_PROGRESION;
-	----------------------------------------------------------------------------------------------
+   end F_OBTENER_PROGRESION;
+   ----------------------------------------------------------------------------------------------
 
-	--------------------------------------------------------------------------
-	function F_OBTENER_DURACION (V_TRAYECTO : in T_TRAYECTO) return Integer is
+   --------------------------------------------------------------------------
+   function F_OBTENER_DURACION (V_TRAYECTO : in T_TRAYECTO) return Integer is
 
-	begin
+   begin
 
-		return V_TRAYECTO.R_DURACION;
+      return V_TRAYECTO.R_DURACION;
 
-	end F_OBTENER_DURACION;
-	--------------------------------------------------------------------------
+   end F_OBTENER_DURACION;
+   --------------------------------------------------------------------------
 
-	---------------------------------------------------------------------------
-        function F_OBTENER_DISTANCIA (V_TRAYECTO : in T_TRAYECTO) return Integer is
+   ---------------------------------------------------------------------------
+   function F_OBTENER_DISTANCIA (V_TRAYECTO : in T_TRAYECTO) return Integer is
 
-        begin
+   begin
 
-                return V_TRAYECTO.R_DISTANCIA;
+      return V_TRAYECTO.R_DISTANCIA;
 
-        end F_OBTENER_DISTANCIA;
-        ---------------------------------------------------------------------------
+   end F_OBTENER_DISTANCIA;
+   ---------------------------------------------------------------------------
 
-	-------------------------------------------------------------------------
-	function F_OBTENER_ESTADO (V_TRAYECTO : in T_TRAYECTO) return T_ESTADO is
+   -------------------------------------------------------------------------
+   function F_OBTENER_ESTADO (V_TRAYECTO : in T_TRAYECTO) return T_ESTADO is
 
-	begin
+   begin
 
-		return V_TRAYECTO.R_ESTADO;
+      return V_TRAYECTO.R_ESTADO;
 
-	end F_OBTENER_ESTADO;
-	-------------------------------------------------------------------------
+   end F_OBTENER_ESTADO;
+   -------------------------------------------------------------------------
 
-	------------------------------------------------------------
-	procedure P_PONER_ESTADO (V_ESTADO : in T_ESTADO;
-				  V_TRAYECTO : in out T_TRAYECTO) is
+   ------------------------------------------------------------
+   procedure P_PONER_ESTADO (V_ESTADO : in T_ESTADO;
+                             V_TRAYECTO : in out T_TRAYECTO) is
 
-	begin
+   begin
 
-		V_TRAYECTO.R_ESTADO := V_ESTADO;
+      V_TRAYECTO.R_ESTADO := V_ESTADO;
 
-	end P_PONER_ESTADO;
-	------------------------------------------------------------
+   end P_PONER_ESTADO;
+   ------------------------------------------------------------
 
-	---------------------------------------------------------------------------------
-	function F_ESTA_TRAYECTO_TERMINADO (V_TRAYECTO : in T_TRAYECTO) return Boolean is
+   ---------------------------------------------------------------------------------
+   function F_ESTA_TRAYECTO_TERMINADO (V_TRAYECTO : in T_TRAYECTO) return Boolean is
 
-	begin
+   begin
 
-		return V_TRAYECTO.R_ESTADO = E_TERMINADO;
+      return V_TRAYECTO.R_ESTADO = E_TERMINADO;
 
-	end F_ESTA_TRAYECTO_TERMINADO;
-	---------------------------------------------------------------------------------
+   end F_ESTA_TRAYECTO_TERMINADO;
+   ---------------------------------------------------------------------------------
 
-	------------------------------------------------------------------------------------------------------
-        function F_OBTENER_VELOCIDAD_ACTUAL (V_TRAYECTO : in T_TRAYECTO) return Q_TIPOS_BASICOS.T_VELOCIDAD is
+   ------------------------------------------------------------------------------------------------------
+   function F_OBTENER_VELOCIDAD_ACTUAL (V_TRAYECTO : in T_TRAYECTO) return Q_TIPOS_BASICOS.T_VELOCIDAD is
 
-        begin
+   begin
 
-                return V_TRAYECTO.R_VELOCIDAD_ACTUAL;
+      return V_TRAYECTO.R_VELOCIDAD_ACTUAL;
 
-        end F_OBTENER_VELOCIDAD_ACTUAL;
-        ------------------------------------------------------------------------------------------------------
+   end F_OBTENER_VELOCIDAD_ACTUAL;
+   ------------------------------------------------------------------------------------------------------
 
-	--------------------------------------------------------------------------------------------------------
-        function F_OBTENER_POSICION_ACTUAL (V_TRAYECTO : in T_TRAYECTO) return Q_TIPOS_BASICOS.T_POSICION_UTM is
+   --------------------------------------------------------------------------------------------------------
+   function F_OBTENER_POSICION_ACTUAL (V_TRAYECTO : in T_TRAYECTO) return Q_TIPOS_BASICOS.T_POSICION_UTM is
 
-        begin
+   begin
 
-                return V_TRAYECTO.R_POSICION_ACTUAL;
+      return V_TRAYECTO.R_POSICION_ACTUAL;
 
-        end F_OBTENER_POSICION_ACTUAL;
-        --------------------------------------------------------------------------------------------------------
+   end F_OBTENER_POSICION_ACTUAL;
+   --------------------------------------------------------------------------------------------------------
 
-	-------------------------------------------------------------------------------------
-	function F_OBTENER_TIEMPO_TRANSCURRIDO (V_TRAYECTO : in T_TRAYECTO) return Integer is
+   -------------------------------------------------------------------------------------
+   function F_OBTENER_TIEMPO_TRANSCURRIDO (V_TRAYECTO : in T_TRAYECTO) return Integer is
 
-	begin
+   begin
 
-		return V_TRAYECTO.R_TIEMPO_TRANSCURRIDO;
+      return V_TRAYECTO.R_TIEMPO_TRANSCURRIDO;
 
-	end F_OBTENER_TIEMPO_TRANSCURRIDO;
-	-------------------------------------------------------------------------------------
+   end F_OBTENER_TIEMPO_TRANSCURRIDO;
+   -------------------------------------------------------------------------------------
 
-	----------------------------------------------------------------------------------------
-	function F_OBTENER_DISTANCIA_POR_RECORRER (V_TRAYECTO : in T_TRAYECTO) return Float is
+   ----------------------------------------------------------------------------------------
+   function F_OBTENER_DISTANCIA_POR_RECORRER (V_TRAYECTO : in T_TRAYECTO) return Float is
 
-	begin
+   begin
 
-		return V_TRAYECTO.R_DISTANCIA_POR_RECORRER;
+      return V_TRAYECTO.R_DISTANCIA_POR_RECORRER;
 
-	end F_OBTENER_DISTANCIA_POR_RECORRER;
-	----------------------------------------------------------------------------------------
+   end F_OBTENER_DISTANCIA_POR_RECORRER;
+   ----------------------------------------------------------------------------------------
+   
+   ---------------------------------------------------------------------------------
+   function F_OBTENER_TRAMO_ACTUAL_ID (V_TRAYECTO : in T_TRAYECTO) return Natural is
+      
+   begin
+      
+      return Q_PROGRESION.F_OBTENER_ID_TRAMO_ACTUAL (Q_PROGRESION.F_OBTENER_ELEMENTO_ACTUAL_PROGRESION (V_TRAYECTO.R_PROGRESION));
+        
+   end F_OBTENER_TRAMO_ACTUAL_ID;
+   ---------------------------------------------------------------------------------
+   
+   -------------------------------------------------------------------------------
+   function F_OBTENER_CARRIL_ACTUAL (V_TRAYECTO : in T_TRAYECTO) return Natural is
+      
+   begin
+      
+      return V_TRAYECTO.R_CARRIL_ACTUAL;
+      
+   end F_OBTENER_CARRIL_ACTUAL;
+   -------------------------------------------------------------------------------
 
-	-----------------------------------------------------------
-	procedure P_CREAR_TRAYECTO (V_VEHICULO : in Q_VEHICULO.T_VEHICULO;
-				    V_POSICION_ORIGEN : in Q_TIPOS_BASICOS.T_POSICION_UTM;
-                                    V_POSICION_FINAL : in Q_TIPOS_BASICOS.T_POSICION_UTM;
-                                    V_HORA_SALIDA : in Ada.Calendar.Time := Ada.Calendar.Clock;
-                                    V_DURACION : in Integer;
-                                    V_DISTANCIA : in Integer;
-				    V_RUTA : in Q_RUTA.T_RUTA;
-                                    V_TRAYECTO : out T_TRAYECTO) is
+   -----------------------------------------------------------
+   procedure P_CREAR_TRAYECTO (V_VEHICULO : in Q_VEHICULO.T_VEHICULO;
+                               V_POSICION_ORIGEN : in Q_TIPOS_BASICOS.T_POSICION_UTM;
+                               V_POSICION_FINAL : in Q_TIPOS_BASICOS.T_POSICION_UTM;
+                               V_HORA_SALIDA : in Ada.Calendar.Time := Ada.Calendar.Clock;
+                               V_DURACION : in Integer;
+                               V_DISTANCIA : in Integer;
+                               V_RUTA : in Q_RUTA.T_RUTA;
+                               V_TRAYECTO : out T_TRAYECTO) is
 
-	begin
+   begin
 
-		V_TRAYECTO.R_ID := Q_VEHICULO.F_OBTENER_MATRICULA (V_VEHICULO);
+      V_TRAYECTO.R_ID := Q_VEHICULO.F_OBTENER_MATRICULA (V_VEHICULO);
 
-		V_TRAYECTO.R_POSICION_ORIGEN := V_POSICION_ORIGEN;
+      V_TRAYECTO.R_POSICION_ORIGEN := V_POSICION_ORIGEN;
 
-		V_TRAYECTO.R_ID_TRAMO_ORIGEN := Q_TRAMO.F_OBTENER_ID (Q_RUTA.Q_LISTA_TRAMOS.F_DEVOLVER_ELEMENTO (V_POSICION => 1,
-														 V_LISTA => V_RUTA));
+      V_TRAYECTO.R_ID_TRAMO_ORIGEN := Q_TRAMO.F_OBTENER_ID (Q_RUTA.Q_LISTA_TRAMOS.F_DEVOLVER_ELEMENTO (V_POSICION => 1,
+                                                                                                       V_LISTA => V_RUTA));
 
-		V_TRAYECTO.R_POSICION_FINAL := V_POSICION_FINAL;
+      V_TRAYECTO.R_POSICION_FINAL := V_POSICION_FINAL;
 
-		V_TRAYECTO.R_ID_TRAMO_DESTINO := 
-			Q_TRAMO.F_OBTENER_ID 
-				(Q_RUTA.Q_LISTA_TRAMOS.F_DEVOLVER_ELEMENTO 
-					(V_POSICION => Q_RUTA.Q_LISTA_TRAMOS.F_CUANTOS_ELEMENTOS (V_RUTA),
-					 V_LISTA => V_RUTA));
+      V_TRAYECTO.R_ID_TRAMO_DESTINO := 
+        Q_TRAMO.F_OBTENER_ID 
+          (Q_RUTA.Q_LISTA_TRAMOS.F_DEVOLVER_ELEMENTO 
+             (V_POSICION => Q_RUTA.Q_LISTA_TRAMOS.F_CUANTOS_ELEMENTOS (V_RUTA),
+              V_LISTA => V_RUTA));
 
-		V_TRAYECTO.R_HORA_SALIDA := V_HORA_SALIDA;
+      V_TRAYECTO.R_HORA_SALIDA := V_HORA_SALIDA;
 		
-		V_TRAYECTO.R_DURACION := V_DURACION;
+      V_TRAYECTO.R_DURACION := V_DURACION;
 
-		V_TRAYECTO.R_DISTANCIA := V_DISTANCIA;
+      V_TRAYECTO.R_DISTANCIA := V_DISTANCIA;
 
-		Q_PROGRESION.P_GENERAR_PROGRESION (V_POSICION_INICIAL => V_POSICION_ORIGEN,
-						   V_POSICION_FINAL => V_POSICION_FINAL,
-						   V_RUTA => V_RUTA,
-						   V_PROGRESION => V_TRAYECTO.R_PROGRESION,
-						   V_PROGRESION_CARRILES_OPTIMO => V_TRAYECTO.R_PROGRESION_CARRILES_OPTIMO,
-						   V_LISTA_TRAMOS_GLORIETA => V_TRAYECTO.R_TRAMOS_EN_GLORIETA);
+      Q_PROGRESION.P_GENERAR_PROGRESION (V_POSICION_INICIAL => V_POSICION_ORIGEN,
+                                         V_POSICION_FINAL => V_POSICION_FINAL,
+                                         V_RUTA => V_RUTA,
+                                         V_PROGRESION => V_TRAYECTO.R_PROGRESION,
+                                         V_PROGRESION_CARRILES_OPTIMO => V_TRAYECTO.R_PROGRESION_CARRILES_OPTIMO,
+                                         V_LISTA_TRAMOS_GLORIETA => V_TRAYECTO.R_TRAMOS_EN_GLORIETA);
 
-		Q_PROGRESION.P_GENERAR_PROGRESION_CARRILES (V_RUTA => V_RUTA,
-							    V_PROGRESION_CARRILES => V_TRAYECTO.R_PROGRESION_CARRILES);
+      Q_PROGRESION.P_GENERAR_PROGRESION_CARRILES (V_RUTA => V_RUTA,
+                                                  V_PROGRESION_CARRILES => V_TRAYECTO.R_PROGRESION_CARRILES);
+      
+      -- Al crear el trayecto el tramo actual es el tramo de origen.
+      V_TRAYECTO.R_TRAMO_ACTUAL_ID := V_TRAYECTO.R_ID_TRAMO_ORIGEN;
 
-		-- Al crear el trayecto la posicion actual sera la de origen.
-		V_TRAYECTO.R_POSICION_ACTUAL := V_POSICION_ORIGEN;
+      -- Al crear el trayecto la posicion actual sera la de origen.
+      V_TRAYECTO.R_POSICION_ACTUAL := V_POSICION_ORIGEN;
 
-		-- Al crear el trayecto (de momento de asume que siempre se parte de parado) al velocidad inicial sera 0.
-		V_TRAYECTO.R_VELOCIDAD_ACTUAL := 0;
+      -- Al crear el trayecto (de momento de asume que siempre se parte de parado) al velocidad inicial sera 0.
+      V_TRAYECTO.R_VELOCIDAD_ACTUAL := 0;
 
-		-- Al crear el trayecto el tiempo trasncurrido se pondra a 0.
-		V_TRAYECTO.R_TIEMPO_TRANSCURRIDO := 0;
+      -- Al crear el trayecto el tiempo trasncurrido se pondra a 0.
+      V_TRAYECTO.R_TIEMPO_TRANSCURRIDO := 0;
+      
+      -- Al crear el trayecto la distancia recorrida se pondra a 0.
+      V_TRAYECTO.R_DISTANCIA_RECORRIDA := 0.0;
 
-		-- Al crear el trayecto se asume que la distancia por recorrer el las distancia de la ruta.
-		V_TRAYECTO.R_DISTANCIA_POR_RECORRER := Float(V_DISTANCIA);	
+      -- Al crear el trayecto se asume que la distancia por recorrer el las distancia de la ruta.
+      V_TRAYECTO.R_DISTANCIA_POR_RECORRER := Float(V_DISTANCIA);	
 
-	end P_CREAR_TRAYECTO;
-	-----------------------------------------------------------
+   end P_CREAR_TRAYECTO;
+   -----------------------------------------------------------
 
 end Q_TRAYECTO;
 --------------------------------------------------------------------------------------------------------------------------------------------
--- dinamico/q_trayecto.ads
+++ dinamico/q_trayecto.ads
@@ -19,123 +19,130 @@ with Q_PROGRESION;
 
 package Q_TRAYECTO is
 
-	type T_ESTADO is (E_PASIVO, E_ACTIVO, E_TERMINADO);
+   type T_ESTADO is (E_PASIVO, E_ACTIVO, E_TERMINADO);
 
-	type T_TRAYECTO is private;
+   type T_TRAYECTO is private;
 
-	function F_OBTENER_ID (V_TRAYECTO : in T_TRAYECTO) return String;
+   function F_OBTENER_ID (V_TRAYECTO : in T_TRAYECTO) return String;
 
-	function F_OBTENER_POSICION_ORIGEN (V_TRAYECTO : in T_TRAYECTO) return Q_TIPOS_BASICOS.T_POSICION_UTM;
+   function F_OBTENER_POSICION_ORIGEN (V_TRAYECTO : in T_TRAYECTO) return Q_TIPOS_BASICOS.T_POSICION_UTM;
 
-	function F_OBTENER_POSICION_FINAL (V_TRAYECTO : in T_TRAYECTO) return Q_TIPOS_BASICOS.T_POSICION_UTM;
+   function F_OBTENER_POSICION_FINAL (V_TRAYECTO : in T_TRAYECTO) return Q_TIPOS_BASICOS.T_POSICION_UTM;
 
-	function F_OBTENER_HORA_SALIDA (V_TRAYECTO : in T_TRAYECTO) return Ada.Calendar.Time;
+   function F_OBTENER_HORA_SALIDA (V_TRAYECTO : in T_TRAYECTO) return Ada.Calendar.Time;
 
-	function F_OBTENER_PROGRESION (V_TRAYECTO : in T_TRAYECTO) return Q_PROGRESION.T_PROGRESION;
+   function F_OBTENER_PROGRESION (V_TRAYECTO : in T_TRAYECTO) return Q_PROGRESION.T_PROGRESION;
 
-	function F_OBTENER_DURACION (V_TRAYECTO : in T_TRAYECTO) return Integer;
+   function F_OBTENER_DURACION (V_TRAYECTO : in T_TRAYECTO) return Integer;
 
-	function F_OBTENER_DISTANCIA (V_TRAYECTO : in T_TRAYECTO) return Integer;
+   function F_OBTENER_DISTANCIA (V_TRAYECTO : in T_TRAYECTO) return Integer;
 
-	function F_OBTENER_ESTADO (V_TRAYECTO : in T_TRAYECTO) return T_ESTADO;
+   function F_OBTENER_ESTADO (V_TRAYECTO : in T_TRAYECTO) return T_ESTADO;
 
-	procedure P_PONER_ESTADO (V_ESTADO : in T_ESTADO;
-				  V_TRAYECTO : in out T_TRAYECTO);
+   procedure P_PONER_ESTADO (V_ESTADO : in T_ESTADO;
+                             V_TRAYECTO : in out T_TRAYECTO);
 
-	function F_ESTA_TRAYECTO_TERMINADO (V_TRAYECTO : in T_TRAYECTO) return Boolean;
+   function F_ESTA_TRAYECTO_TERMINADO (V_TRAYECTO : in T_TRAYECTO) return Boolean;
 
-	function F_OBTENER_VELOCIDAD_ACTUAL (V_TRAYECTO : in T_TRAYECTO) return Q_TIPOS_BASICOS.T_VELOCIDAD;
+   function F_OBTENER_VELOCIDAD_ACTUAL (V_TRAYECTO : in T_TRAYECTO) return Q_TIPOS_BASICOS.T_VELOCIDAD;
 
-	function F_OBTENER_POSICION_ACTUAL (V_TRAYECTO : in T_TRAYECTO) return Q_TIPOS_BASICOS.T_POSICION_UTM;
+   function F_OBTENER_POSICION_ACTUAL (V_TRAYECTO : in T_TRAYECTO) return Q_TIPOS_BASICOS.T_POSICION_UTM;
 
-	function F_OBTENER_TIEMPO_TRANSCURRIDO (V_TRAYECTO : in T_TRAYECTO) return Integer;
+   function F_OBTENER_TIEMPO_TRANSCURRIDO (V_TRAYECTO : in T_TRAYECTO) return Integer;
 
-	function F_OBTENER_DISTANCIA_POR_RECORRER (V_TRAYECTO : in T_TRAYECTO) return Float;
+   function F_OBTENER_DISTANCIA_POR_RECORRER (V_TRAYECTO : in T_TRAYECTO) return Float;
+   
+   function F_OBTENER_TRAMO_ACTUAL_ID (V_TRAYECTO : in T_TRAYECTO) return Natural;
+   
+   function F_OBTENER_CARRIL_ACTUAL (V_TRAYECTO : in T_TRAYECTO) return Natural;
 
-	procedure P_CREAR_TRAYECTO (V_VEHICULO : in Q_VEHICULO.T_VEHICULO;
-				    V_POSICION_ORIGEN : in Q_TIPOS_BASICOS.T_POSICION_UTM;
-				    V_POSICION_FINAL : in Q_TIPOS_BASICOS.T_POSICION_UTM;
-				    V_HORA_SALIDA : in Ada.Calendar.Time := Ada.Calendar.Clock;
-				    V_DURACION : in Integer;
-				    V_DISTANCIA : in Integer;
-				    V_RUTA : in Q_RUTA.T_RUTA;
-				    V_TRAYECTO : out T_TRAYECTO);
+   procedure P_CREAR_TRAYECTO (V_VEHICULO : in Q_VEHICULO.T_VEHICULO;
+                               V_POSICION_ORIGEN : in Q_TIPOS_BASICOS.T_POSICION_UTM;
+                               V_POSICION_FINAL : in Q_TIPOS_BASICOS.T_POSICION_UTM;
+                               V_HORA_SALIDA : in Ada.Calendar.Time := Ada.Calendar.Clock;
+                               V_DURACION : in Integer;
+                               V_DISTANCIA : in Integer;
+                               V_RUTA : in Q_RUTA.T_RUTA;
+                               V_TRAYECTO : out T_TRAYECTO);
 
-	private
+private
 
-		-- Tipo para el trayecto.
-		-- 4.- Distancia que queda por recorrer.
-		type T_TRAYECTO is record
+   -- Tipo para el trayecto.
+   -- 4.- Distancia que queda por recorrer.
+   type T_TRAYECTO is record
 
-			-- Parte estatica
+      -- Parte estatica
 
-			-- ID: Se correspondera con la matricula del vehiculo.
-			-- TO DO: Crear el tipo matricula dentro del vehiculo y no usar un tipo string.
-			-- Hacerlo cuando funcione la progresion.
-			R_ID : String (1..8);
+      -- ID: Se correspondera con la matricula del vehiculo.
+      -- TO DO: Crear el tipo matricula dentro del vehiculo y no usar un tipo string.
+      -- Hacerlo cuando funcione la progresion.
+      R_ID : String (1..8);
 
-			-- Punto de partida
-			R_POSICION_ORIGEN : Q_TIPOS_BASICOS.T_POSICION_UTM;
+      -- Punto de partida
+      R_POSICION_ORIGEN : Q_TIPOS_BASICOS.T_POSICION_UTM;
 
-			-- Tramo de partida
-			R_ID_TRAMO_ORIGEN : Integer;
+      -- Tramo de partida
+      R_ID_TRAMO_ORIGEN : Integer;
 
-			-- Punto de llegada.
-			R_POSICION_FINAL : Q_TIPOS_BASICOS.T_POSICION_UTM;
+      -- Punto de llegada.
+      R_POSICION_FINAL : Q_TIPOS_BASICOS.T_POSICION_UTM;
 
-			-- Tramo de destino
-			R_ID_TRAMO_DESTINO : Integer;
+      -- Tramo de destino
+      R_ID_TRAMO_DESTINO : Integer;
 
-			-- Hora de salida.
-			R_HORA_SALIDA : Ada.Calendar.Time;
+      -- Hora de salida.
+      R_HORA_SALIDA : Ada.Calendar.Time;
 
-			-- Duracion del trayecto.
-			R_DURACION : Integer;
+      -- Duracion del trayecto.
+      R_DURACION : Integer;
 
-			-- Distancia del trayecto.
-			R_DISTANCIA : Integer;
+      -- Distancia del trayecto.
+      R_DISTANCIA : Integer;
 
-			-- Parte dinamica
+      -- Parte dinamica
 			
-			-- Estado del trayecto. Por defecto al crear un trayecto el estado es pasivo (el vehiculo no ha arrancado).
-			R_ESTADO : T_ESTADO := E_PASIVO;
+      -- Estado del trayecto. Por defecto al crear un trayecto el estado es pasivo (el vehiculo no ha arrancado).
+      R_ESTADO : T_ESTADO := E_PASIVO;
 
-			-- Progresion que queda por recorrer.
-                        R_PROGRESION : Q_PROGRESION.T_PROGRESION;
+      -- Progresion que queda por recorrer.
+      R_PROGRESION : Q_PROGRESION.T_PROGRESION;
 
-			-- Progresion de carriles (a traves de que carriles se conectan los tramos de la ruta)
-			R_PROGRESION_CARRILES : Q_PROGRESION.T_PROGRESION_CARRILES;
+      -- Progresion de carriles (a traves de que carriles se conectan los tramos de la ruta)
+      R_PROGRESION_CARRILES : Q_PROGRESION.T_PROGRESION_CARRILES;
 
-			-- Progresion de carriles optimo.
-			R_PROGRESION_CARRILES_OPTIMO : Q_PROGRESION.T_PROGRESION_CARRILES_OPTIMO;
+      -- Progresion de carriles optimo.
+      R_PROGRESION_CARRILES_OPTIMO : Q_PROGRESION.T_PROGRESION_CARRILES_OPTIMO;
 
-			-- Lista de tramos que pertenecen a una rotonda en la que se pueda cambiar de carril.
-			R_TRAMOS_EN_GLORIETA : Q_PROGRESION.T_LISTA_TRAMOS_GLORIETA;
+      -- Lista de tramos que pertenecen a una rotonda en la que se pueda cambiar de carril.
+      R_TRAMOS_EN_GLORIETA : Q_PROGRESION.T_LISTA_TRAMOS_GLORIETA;
+      
+      -- Tramo actual.
+      R_TRAMO_ACTUAL_ID : Integer range 0 .. Q_TRAMO.F_OBTENER_NUMERO_MAXIMO_TRAMOS := 0;
 
-			-- Numero que identifica el carril dentro del tramo en el que se encuentra el vehiculo.
-			-- Siendo 1 el carril de la derecha.
-			R_CARRIL_ACTUAL : Natural range 1 .. 3 := 1;
+      -- Numero que identifica el carril dentro del tramo en el que se encuentra el vehiculo.
+      -- Siendo 1 el carril de la derecha.
+      R_CARRIL_ACTUAL : Natural range 1 .. 3 := 1;
 
-			-- Velocidad actual del vehiculo.
-                        R_VELOCIDAD_ACTUAL : Q_TIPOS_BASICOS.T_VELOCIDAD;
+      -- Velocidad actual del vehiculo.
+      R_VELOCIDAD_ACTUAL : Q_TIPOS_BASICOS.T_VELOCIDAD;
 
-			-- Posicion actual
-			R_POSICION_ACTUAL : Q_TIPOS_BASICOS.T_POSICION_UTM;
+      -- Posicion actual
+      R_POSICION_ACTUAL : Q_TIPOS_BASICOS.T_POSICION_UTM;
 
-			-- Tiempo transcurrido desde el comienzo del trayecto.
-			R_TIEMPO_TRANSCURRIDO : Integer := 0;
+      -- Tiempo transcurrido desde el comienzo del trayecto.
+      R_TIEMPO_TRANSCURRIDO : Integer := 0;
 
-			-- Distancia que queda por recorrer.
-			R_DISTANCIA_POR_RECORRER : Float;
+      -- Distancia que queda por recorrer.
+      R_DISTANCIA_POR_RECORRER : Float;
 
-			R_DISTANCIA_RECORRIDA : Float := 0.0;
+      R_DISTANCIA_RECORRIDA : Float := 0.0;
 
-			R_SEGMENTOS_AVANZADOS : Natural := 0;
+      R_SEGMENTOS_AVANZADOS : Natural := 0;
 
-			-- Distancia acarreo. La diferencia entre la distancia recorrida y el numero de segmentos a avanzar.
-			R_ACARREO : Float := 0.0;	
+      -- Distancia acarreo. La diferencia entre la distancia recorrida y el numero de segmentos a avanzar.
+      R_ACARREO : Float := 0.0;	
 
-		end record;
+   end record;
 
 end Q_TRAYECTO;
 ---------------
